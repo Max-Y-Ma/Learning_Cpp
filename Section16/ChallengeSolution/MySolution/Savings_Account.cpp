200e441c409587d31f953839b6eba02c1776c680
Max Ma <maxma2@illinois.edu>
2022-08-23 11:47:17 -0500

    Completed Section 16

--- Section16/ChallengeSolution/MySolution/Savings_Account.cpp
+++ Section16/ChallengeSolution/MySolution/Savings_Account.cpp
@@ -4,17 +4,18 @@ Savings_Account::Savings_Account(std::string name, double balance, double int_ra
     : Account {name, balance}, int_rate{int_rate} {
 }
 
-// Deposit:
-//      Amount supplied to deposit will be incremented by (amount * int_rate/100) 
-//      and then the updated amount will be deposited
-//
 bool Savings_Account::deposit(double amount) {
     amount += amount * (int_rate/100);
     return Account::deposit(amount);
 }
 
-std::ostream &operator<<(std::ostream &os, const Savings_Account &account) {
-    os << "[Savings_Account: " << account.name << ": " << account.balance << ", " << account.int_rate << "]";
-    return os;
+bool Savings_Account::withdraw(double amount) {
+    return Account::withdraw(amount);
 }
+
+void Savings_Account::print(std::ostream &os) const {
+    os << "[Savings Account: " << this->name << ": " << this->balance  << ": " << this->int_rate << "]";
+}
+
+
 
