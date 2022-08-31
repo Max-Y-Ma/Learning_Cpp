767401cedcf75c76761292339bb3d4d7c9ce7a6a
Max Ma <maxma2@illinois.edu>
2022-08-31 13:14:51 -0500

    Completed Section19

--- Section19/Challenge_1_Solution/MySolution/main.cpp
+++ Section19/Challenge_1_Solution/MySolution/main.cpp
@@ -1,6 +1,34 @@
 // Section 19
-// Challenge 1
-// Formatting output
+/*
+Section 19 
+Challenge 1 
+
+Formatting output
+===========================================================
+I have provided you with the starter source code for this challenge.
+In this challenge you are to display the provided data in a nicely formatted table.
+Your table does not have to match mine, but practice with the format manipulators
+so you become comfortable with them.
+
+There are 3 structures provided. Yes, these could also be classes with public data, 
+but it's common to use structures when we simply want to model only data and not 
+operations as we would with classes.
+
+The structures work together to create a Tours structure that will contain
+information about Tours to South America.
+The Tours include Countries, Cities within those countries along with population and cost data for 
+each city tour.
+I know, population? Huh? I just wanted a big number so that you can format it, OK :)
+
+Please watch the Challenge video for the expected output. It won't look right if I put it in this
+editor because of the font style.
+
+In the provided source code I'm giving you some code that displays the data from the structures 
+and the vectors within the structures. This code uses tabs to format the data and it looks pretty ugly.
+
+Have fun!! Create functions if you like, use constants, whatever you think makes sense!
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -58,13 +86,16 @@ int main()
 
     // Unformatted display so you can see how to access the vector elements
     std::cout << tours.title << std::endl;
+    std::cout << std::setfill('-') << std::setw(65) << ' ' << std::setfill(' ') << std::endl;
+    std::cout << "Country" << std::setw(23) << "City" << std::setw(15) << "Population" << std::setw(15) << "Price" << std::endl;
+    std::cout << std::setfill('-') << std::setw(65) << ' ' << std::setfill(' ') << std::endl;
     for(auto country : tours.countries) {   // loop through the countries
         std::cout << country.name << std::endl;
         for(auto city : country.cities) {       // loop through the cities for each country
-            std::cout << "\t" << city.name 
-                          << "\t" << city.population 
-                          << "\t" << city.cost 
-                          << std::endl;
+            std::cout << std::setw(30) << city.name 
+                      << std::scientific << std::setw(15) << city.population 
+                      << std::setw(10) << std::resetiosflags(std::ios::scientific) <<  std::setprecision(2) << std::fixed << "$" << city.cost 
+                      << std::endl;
         }
     }
 
--- Section19/Challenge_4_Solution/MySolution/main.cpp
+++ Section19/Challenge_4_Solution/MySolution/main.cpp
@@ -1,10 +1,56 @@
 // Section 19
-// Challenge 4
-// Copy Romeo and Juliet with line numbers
+/*
+Section 19
+Challenge 4
+Copy Romeo and Juliet with line numbers
+
+This challenge should be pretty easy.
+I have provided the text to Romeo and Juliet in a file named romeoandjuliet.txt
+
+For this challenge you have to make copy of the Romeo and Juliet file and save it to another file.
+This new copy should have line numbers at the beginning of each line in the play.
+Please format the numbers nicely so everything lines up.
+
+Feel free to extend this challenge in any way you like.
+For example, you may want to provide line numbers only for lines that actually have text
+
+Have fun!
+*/
+
 #include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
 int main() {
-    
+    ifstream in_file;
+    ofstream out_file;
+    in_file.open("./romeoandjuliet");
+    out_file.open("./romeoandjulietlines", ios::app);
+
+    if (!in_file || !out_file) {
+        cerr << "Trouble Opening Files!" << endl;
+        return 1;
+    }
+
+    out_file.clear();
+    unsigned long int num_line {1};
+    while (!in_file.eof()) {
+        string line;
+        getline(in_file, line);
+
+        if (line.compare("\r") == 0) {
+            out_file << endl;
+            continue;
+        }
+
+        out_file << num_line << ": " << line << endl;
+        num_line++;
+    }
+
+    in_file.close();
+    out_file.close();
+
     return 0;
 }
 
